b6a8b8ace00acf4d5c865eef9634c2b71a179ead
bssthu <bssda03@gmail.com>
2014-07-11 23:39:57 +0800

    fix compile errors

--- followme_ctrl/src/followme_ctrl_node.cpp
+++ followme_ctrl/src/followme_ctrl_node.cpp
@@ -12,8 +12,8 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "followme_ctrl");
     ros::NodeHandle nh;
-    state_pub = nh.advertise<frmsg::followme_state>("followme_state", 10);
-    people_sub = nh.subscribe("followme_people", 10, &peopleCallback);
+    ros::Publisher state_pub = nh.advertise<frmsg::followme_state>("followme_state", 10);
+    ros::Subscriber people_sub = nh.subscribe("followme_people", 10, &peopleCallback);
     ros::spin();
     return 0;
 }
