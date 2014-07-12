0e08de25965edea026d1c1d7c59ff1f2d3107dcf
bssthu <bssda03@gmail.com>
2014-07-12 22:23:33 +0800

    merge from xf, navigation ok.

--- optic_flow/src/main.cpp
+++ optic_flow/src/main.cpp
@@ -10,21 +10,21 @@
 #include "std_msgs/MultiArrayLayout.h"
 #include "std_msgs/MultiArrayDimension.h"
 #include "std_msgs/UInt8MultiArray.h"
-
+ros::Publisher * pub;
 using namespace std;
 void Opt_Deltamove(const char opt[8],float* dx,float* dy,float* dth)
 {
 float vx,vy,tx,ty,fx1,fx2,fy1,fy2;
 int x1,x2,y2,y1;
-for (int i = 0;i<8;i++)
-cout<<(int)opt[i]<<' ';
-cout<<endl;
+//for (int i = 0;i<8;i++)
+//cout<<(int)opt[i]<<' ';
+//cout<<endl;
 
 x1=(((unsigned char)opt[1])<<8)+(unsigned char)opt[0];
 y1=(((unsigned char)opt[3])<<8)+(unsigned char)opt[2];
 x2=(((unsigned char)opt[5])<<8)+(unsigned char)opt[4];
 y2=(((unsigned char)opt[7])<<8)+(unsigned char)opt[6];
-cout<<(int)opt[0]<<' '<<(int)opt[1]<<' '<<(int)opt[2]<<' '<<(int)opt[3]<<' '<<(int)opt[4]<<' '<<(int)opt[5]<<' '<<(int)opt[6]<<' '<<(int)opt[7]<<endl;
+//cout<<(int)opt[0]<<' '<<(int)opt[1]<<' '<<(int)opt[2]<<' '<<(int)opt[3]<<' '<<(int)opt[4]<<' '<<(int)opt[5]<<' '<<(int)opt[6]<<' '<<(int)opt[7]<<endl;
 //cout<<x1<<' '<<x2<<' '<<y1<<' '<<y2<<endl;
 if(x1&0x8000)
 	x1=-((x1^0xffff)+1);
@@ -35,10 +35,10 @@ if(y1&0x8000)
 if(y2&0x8000)
 	y2=-((y2^0xffff)+1);
 
-fx1=(float)x1/OPTSCALE;
-fx2=(float)x2/OPTSCALE;
-fy1=(float)y1/OPTSCALE;
-fy2=(float)y2/OPTSCALE;
+fx1=-(float)x1/OPTSCALE;
+fx2=-(float)x2/OPTSCALE;
+fy1=-(float)y1/OPTSCALE;
+fy2=-(float)y2/OPTSCALE;
 
 ty=(fy1-fy2)/2;
 vy=(fy1+fy2)/2;
@@ -64,10 +64,8 @@ int main(int argc, char ** argv)
     ros::init(argc, argv, "talker");
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("opticflow",1000,opticCallback);
+    
     ros::spin();
-    return 0;
-
-
     return 0;
 }
 
