6b527b15ce5c462484acf33a9bb165157ffba40a
badpoet <badpoet55@gmail.com>
2014-07-21 20:05:18 +0800

    path

--- inspection_ctrl/include/inspection_ctrl/inspection_ctrl.h
+++ inspection_ctrl/include/inspection_ctrl/inspection_ctrl.h
@@ -16,22 +16,13 @@ public:
     ~InspectionCtrl();
 
 private:
+    void pathInit();
     void nodeInit();
     void navigationInit();
     void doorDetectorCallback(const std_msgs::Int32::ConstPtr &p);
     void walk();
 
-    void set_target() {
-
-     tar_x_[0] = 5.971, tar_x_[1] = 8.672, tar_x_[2] = 15.005;
-     tar_y_[0] = -6.850, tar_y_[1] = -1.227, tar_y_[2] = -8.904;
-     tar_z_[0] = 0.00, tar_z_[1] = 0.00, tar_z_[2] = 0.00;
-     tar_oz_[0] = 0.666, tar_oz_[1] = 0.729, tar_oz_[2] = -0.667;
-     tar_ow_[0] = 0.746, tar_ow_[1] = 0.685, tar_ow_[2] = 0.745;
-
-    }
-
-    double tar_x_[3], tar_y_[3], tar_z_[3], tar_oz_[3], tar_ow_[3];
+    double tar_x_[11], tar_y_[11], tar_oz_[11], tar_ow_[11];
 
     MoveBaseClient ac_;
 
@@ -42,6 +33,7 @@ private:
 
     int open_count;
     int is_moving;
+    int path_len;
 };
 
 const int OPEN_THRES = 10;
--- inspection_ctrl/src/inspection_ctrl.cpp
+++ inspection_ctrl/src/inspection_ctrl.cpp
@@ -8,6 +8,7 @@ InspectionCtrl::InspectionCtrl(ros::NodeHandle nh):
     is_moving(0),
     open_count(0)
 {
+    pathInit();
     nodeInit();
     navigationInit();
 
@@ -15,6 +16,8 @@ InspectionCtrl::InspectionCtrl(ros::NodeHandle nh):
     start_msg.data = 1;
     for (int i = 0; i < 5; i++)
         door_signal_publisher_.publish(start_msg);
+
+    printf("InspectionCtrl Ready!\n");
 }
 
 InspectionCtrl::~InspectionCtrl()
@@ -22,10 +25,67 @@ InspectionCtrl::~InspectionCtrl()
     // nothing
 }
 
+void InspectionCtrl::pathInit()
+{
+    path_len = 11;
+    tar_x_[0] = 6.097;
+    tar_y_[0] = -7.992;
+    tar_oz_[0] = 0.698;
+    tar_ow_[0] = 0.716;
+
+    tar_x_[1] = 6.663;
+    tar_y_[1] = -6.288;
+    tar_oz_[1] = 0.221;
+    tar_ow_[1] = 0.975;
+
+    tar_x_[2] = 7.508;
+    tar_y_[2] = -5.172;
+    tar_oz_[2] = 0.621;
+    tar_ow_[2] = 0.784;
+
+    tar_x_[3] = 8.233;
+    tar_y_[3] = -3.033;
+    tar_oz_[3] = 0.571;
+    tar_ow_[3] = 0.821;
+
+    tar_x_[4] = 9.080;
+    tar_y_[4] = -0.481;
+    tar_oz_[4] = 0.089;
+    tar_ow_[4] = 0.996;
+
+    tar_x_[5] = 11.751;
+    tar_y_[5] = 0.113;
+    tar_oz_[5] = -0.001;
+    tar_ow_[5] = 0.100;
+
+    tar_x_[6] = 12.928;
+    tar_y_[6] = -0.336;
+    tar_oz_[6] = -0.735;
+    tar_ow_[6] = 0.678;
+
+    tar_x_[7] = 13.391;
+    tar_y_[7] = -3.404;
+    tar_oz_[7] = -0.669;
+    tar_ow_[7] = 0.743;
+
+    tar_x_[8] = 13.801;
+    tar_y_[8] = -5.317;
+    tar_oz_[8] = -0.331;
+    tar_ow_[8] = 0.944;
+
+    tar_x_[9] = 14.806;
+    tar_y_[9] = -6.462;
+    tar_oz_[9] = -0.581;
+    tar_ow_[9] = 0.814;
+
+    tar_x_[10] = 14.954;
+    tar_y_[10] = -7.980;
+    tar_oz_[10] = -0.796;
+    tar_ow_[10] = 0.605;
+}
+
 void InspectionCtrl::nodeInit()
 {
-    printf("Inspection Controller set up\n");
-
     door_signal_publisher_ = nh_.advertise< std_msgs:: Int32 >(
         "door_signal", 5);
 
@@ -65,7 +125,7 @@ void InspectionCtrl::walk()
     ros::Duration start_space(2.0);
     start_space.sleep();
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < path_len; i++) {
         move_base_msgs::MoveBaseGoal goal;
 
         goal.target_pose.header.frame_id = "map";
@@ -73,14 +133,14 @@ void InspectionCtrl::walk()
 
         goal.target_pose.pose.position.x = tar_x_[i];
         goal.target_pose.pose.position.y = tar_y_[i];
-        goal.target_pose.pose.position.z = tar_z_[i];
+        goal.target_pose.pose.position.z = 0;
 
         goal.target_pose.pose.orientation.x = 0;
         goal.target_pose.pose.orientation.y = 0;
         goal.target_pose.pose.orientation.z = tar_oz_[i];
         goal.target_pose.pose.orientation.w = tar_ow_[i];
 
-        printf("Sending goal");
+        printf("Sending goal (%lf %lf) (%lf %lf)\n", tar_x_[i], tar_y_[i], tar_oz_[i], tar_ow_[i]);
         ac_.sendGoal(goal);
 
         ac_.waitForResult();
