04b0e72cbe7746c6b8cb12f1cad75d60dc17d0ae
bssthu <bssda03@gmail.com>
2014-07-22 00:53:27 +0800

    serial arm not ok

--- serial/src/arm.cpp
+++ serial/src/arm.cpp
@@ -1,7 +1,7 @@
 // Project:     serial
 // File:        arm.cpp
 // Created by bss at 2014-07-21
-// Last modified: 2014-07-21, 23:08:58
+// Last modified: 2014-07-21, 23:37:11
 // Description:
 ///edited by xf 2014-1-18
 //
@@ -46,7 +46,7 @@ int main(int argc, char** argv)
     cout<<"serial_arm"<<endl;
     ros::init(argc, argv, "serialport_arm_driver");
     ros::NodeHandle n;
-    ros::Rate rate(1000);
+    ros::Rate rate(20);
     std::string package_path = ros::package::getPath("serial") + "/";
     QSettings settings((package_path + "arm.ini").c_str(),QSettings::IniFormat);
     int pos = settings.value("pos", 1).toInt();
@@ -113,6 +113,7 @@ int main(int argc, char** argv)
     
     // read from file
     int recmove[3][1000];
+	int recrest[3][1000];
 	FILE *f1;
 	f1=fopen((package_path + "data/wave2.txt").c_str(), "r");
 	for(int j=0;j<3;j++)
@@ -123,54 +124,151 @@ int main(int argc, char** argv)
 	    }
 	}
 	fclose(f1);
+  	f1=fopen((package_path + "data/rest.txt").c_str(), "r");
+	for(int j=0;j<3;j++)
+	{
+	    for(int i=0;i<384;i++)
+	    {
+	        fscanf(f1,"%d",&(recrest[j][i]));
+	    }
+	}
+	fclose(f1);
 
     unsigned char data[16];
-    ros::Rate loop_rate(1000);
     int speed, sum;
     int lastpos[3];
     int repflag = 0;
+	const int maxmove=315;
+	const int maxrest=383;
     while(ros::ok())
     {
-        for (int rcvack = 1; rcvack <= 3; rcvack++)
-        {
-            data[0]=0xff;
-            data[1]=0xff;
-            data[2]=rcvack;
-            data[3]=0x07;
-            data[4]=0x03;
-            data[5]=0x1e;		
-            data[6]=(unsigned char)(recmove[rcvack-1][repflag]%256);			
-            data[7]=(unsigned char)(recmove[rcvack-1][repflag]/256);
-            speed=(abs(recmove[rcvack-1][repflag]-lastpos[rcvack-1])/2)+1;
-            lastpos[rcvack-1]=recmove[rcvack-1][repflag];		
-            data[8]=(unsigned char)(speed%256);
-            data[9]=(unsigned char)(speed/256);
-            sum=0;
-            for(int i=2;i<10;i++)
-            {
-	            sum+=data[i];
-            }
-            data[10]=sum & 0xff;
-            data[10]=~(data[10]);
+	lastpos[0] = recrest[0][maxrest];
+    lastpos[1] = recrest[1][maxrest];
+    lastpos[2] = recrest[2][maxrest];
+    repflag = maxrest-1;
+	while(repflag>1 && ros::ok())
+		for (int rcvack = 1; rcvack <= 3; rcvack++)
+		{
+		    
+		    data[0]=0xff;
+		    data[1]=0xff;
+		    data[2]=rcvack;
+		    data[3]=0x07;
+		    data[4]=0x03;
+		    data[5]=0x1e;		
+		    data[6]=(unsigned char)(recrest[rcvack-1][repflag]%256);			
+		    data[7]=(unsigned char)(recrest[rcvack-1][repflag]/256);
+		    speed=(abs(recmove[rcvack-1][repflag]-lastpos[rcvack-1])/2)+1;
+		    lastpos[rcvack-1]=recrest[rcvack-1][repflag];		
+		    data[8]=(unsigned char)(speed%256);
+		    data[9]=(unsigned char)(speed/256);
+		    sum=0;
+		    for(int i=2;i<10;i++)
+		    {
+			    sum+=data[i];
+		    }
+		    data[10]=sum & 0xff;
+		    data[10]=~(data[10]);
 
-            
-            //comm1.put_Output(COleVariant(data));
-            for (int i = 0; i <= 10; i++)
-            {
-                my_serial_stream << data[i];
-                std::cout<<std::hex<<(int)data[i]<<" ";
-            }
-            std::cout<<"endl";
-            char ch;
-            std::cin>>ch;
-            printf("one\n");
-            repflag++;
-            
-            loop_rate.sleep();
-        }
-        
-        
-        rate.sleep();
+		    
+		    //comm1.put_Output(COleVariant(data));
+		    for (int i = 0; i <= 10; i++)
+		    {
+		        my_serial_stream << data[i];
+		        std::cout<<std::hex<<(int)data[i]<<" ";
+		    }
+		    std::cout<<endl;
+		    printf("one\n");
+		    repflag--;
+		    
+		    rate.sleep();
+		}
+
+	lastpos[0] = recmove[0][0];
+    lastpos[1] = recmove[1][0];
+    lastpos[2] = recmove[2][0];
+    repflag = 1;
+	while(repflag<maxmove && ros::ok())
+		for (int rcvack = 1; rcvack <= 3; rcvack++)
+		{
+		    
+		    data[0]=0xff;
+		    data[1]=0xff;
+		    data[2]=rcvack;
+		    data[3]=0x07;
+		    data[4]=0x03;
+		    data[5]=0x1e;		
+		    data[6]=(unsigned char)(recmove[rcvack-1][repflag]%256);			
+		    data[7]=(unsigned char)(recmove[rcvack-1][repflag]/256);
+		    speed=(abs(recmove[rcvack-1][repflag]-lastpos[rcvack-1])/2)+1;
+		    lastpos[rcvack-1]=recmove[rcvack-1][repflag];		
+		    data[8]=(unsigned char)(speed%256);
+		    data[9]=(unsigned char)(speed/256);
+		    sum=0;
+		    for(int i=2;i<10;i++)
+		    {
+			    sum+=data[i];
+		    }
+		    data[10]=sum & 0xff;
+		    data[10]=~(data[10]);
+
+		    
+		    //comm1.put_Output(COleVariant(data));
+		    for (int i = 0; i <= 10; i++)
+		    {
+		        my_serial_stream << data[i];
+		        std::cout<<std::hex<<(int)data[i]<<" ";
+		    }
+		    std::cout<<endl;
+		    printf("one\n");
+		    repflag++;
+		    
+		    rate.sleep();
+		}
+	lastpos[0] = recrest[0][1];
+    lastpos[1] = recrest[1][1];
+    lastpos[2] = recrest[2][1];
+    repflag = 2;
+	while(repflag<maxrest-1 && ros::ok())
+		for (int rcvack = 1; rcvack <= 3; rcvack++)
+		{
+		    
+		    data[0]=0xff;
+		    data[1]=0xff;
+		    data[2]=rcvack;
+		    data[3]=0x07;
+		    data[4]=0x03;
+		    data[5]=0x1e;		
+		    data[6]=(unsigned char)(recrest[rcvack-1][repflag]%256);			
+		    data[7]=(unsigned char)(recrest[rcvack-1][repflag]/256);
+		    speed=(abs(recmove[rcvack-1][repflag]-lastpos[rcvack-1])/2)+1;
+		    lastpos[rcvack-1]=recrest[rcvack-1][repflag];		
+		    data[8]=(unsigned char)(speed%256);
+		    data[9]=(unsigned char)(speed/256);
+		    sum=0;
+		    for(int i=2;i<10;i++)
+		    {
+			    sum+=data[i];
+		    }
+		    data[10]=sum & 0xff;
+		    data[10]=~(data[10]);
+
+		    
+		    //comm1.put_Output(COleVariant(data));
+		    for (int i = 0; i <= 10; i++)
+		    {
+		        my_serial_stream << data[i];
+		        std::cout<<std::hex<<(int)data[i]<<" ";
+		    }
+		    std::cout<<endl;
+		    printf("one\n");
+		    repflag--;
+		    
+		    rate.sleep();
+		}
+		ros::Duration(5.0).sleep();		
+ 		       
+        //rate.sleep();
         //msg2.data = ss.str();
         //ROS_INFO("%s",buffer_opt);
         //pub.publish(msg2);
